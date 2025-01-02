ba8d0190c70e28c77a56f366b2db18a5462a596a
Piyush Raghav <156657568+Piyushraghav01@users.noreply.github.com>
2025-01-02 22:13:03 +0530

    Add files via upload

---
+++ OOP.java
@@ -0,0 +1,110 @@
+// Intro and Classes and Objects
+/*
+ import java.util.Scanner;
+
+
+public Class Student{
+    String name;
+    int rollNumber;
+}
+
+public Class StudentUse{
+    public static void main(String[] args){
+        //Scanner s= new Scanner(source)
+        Student s1= new Student();
+        Student s2= new Student();
+        System.out.println(s1);
+        s1.name="Manisha";
+        s1.rollNumber = 10;
+        System.out.println(s1.name + " "+ s1.rollNumber);
+
+        s2.name= "Ankush";
+        s2.rollNumber= 50;
+
+        System.out.println(s2.name + " " + s2.rollNumber);
+    }
+}
+ */
+
+ // Access Modifiles
+ /*
+  // Student.java
+
+// package classesandobjects;
+
+// public class Student{
+
+//     String name;
+//     int rollNumber;
+
+//     void print(){
+//         System.out.println(name + " "+ rollnumber)
+//     }
+// }
+
+
+// StudentUse.java
+
+// package classesandobjects;
+
+// public class StudentUse(){
+//     public static void main (String[] args){
+//         student s1 = new student();
+//         student s2 = new Student();
+
+//         s1.name = "Manisha";
+//         s1.rollNumber= 10;
+//         s2.name="Ankush";
+//         s2.rollNumber=50;
+        
+//         s1.print();
+//         s2.print();
+//     }
+// }
+
+// Main.java
+
+// package classesandobjects;
+
+// public class student{
+//     int rollNumber;
+//     public void print(){
+//         System.out.println(name+" "+rollNumber);
+//     }
+// }
+  */
+
+  // Getters and Setters
+  /*
+   
+   */
+
+   // Dynamic Array class
+   /*
+    package classandobjects;
+
+public class DynamicArray{
+    private int[] data;
+    private int nextElementIndex;
+    public DynamicArray(){
+        data = new int[5];
+    }
+    public int size(){
+        return nextElementIndex;
+    }
+    public int get(int i){
+        if(i>=nextElementIndex){
+            return 0;
+        }
+        return data[i];
+    }
+    public boolean isEmpty(){
+   return nextElementIndex==0;
+    }
+    public void add(int elem){
+        
+    }
+}
+    */
+
+    
